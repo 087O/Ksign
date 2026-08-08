06db9c45e8ac109e9d2cb3a8209481ec30ce7c06
Abdul Basit <m4h4wyh8f9@privaterelay.appleid.com>
2026-08-08 18:32:20 +0300

    STEP 2: Reorder tabs - make App Store the first default tab

--- Ksign/Views/TabView/TabEnum.swift
+++ Ksign/Views/TabView/TabEnum.swift
@@ -9,56 +9,56 @@ import SwiftUI
 import NimbleViews
 
 enum TabEnum: String, CaseIterable, Hashable {
-    case files
+	case files
 	case sources
 	case library
 	case settings
 	case certificates
 	case appstore
-    case downloader
+	case downloader
 	var title: String {
 		switch self {
-        case .files:        return .localized("Files")
+		case .files:        return .localized("Files")
 		case .sources:     	return .localized("Sources")
 		case .library: 		return .localized("Library")
 		case .settings: 	return .localized("Settings")
 		case .certificates:	return .localized("Certificates")
 		case .appstore: 	return .localized("App Store")
-        case .downloader:   return .localized("Downloads")
+		case .downloader:   return .localized("Downloads")
 		}
 	}
 	
 	var icon: String {
 		switch self {
-        case .files:        return "folder.fill"
+		case .files:        return "folder.fill"
 		case .sources: 		return "globe.desk"
 		case .library: 		return "square.grid.2x2"
 		case .settings: 	return "gearshape.2"
 		case .certificates: return "person.text.rectangle"
 		case .appstore: 	return "plus.app.fill"
-        case .downloader:    return "square.and.arrow.down.fill"
+		case .downloader:    return "square.and.arrow.down.fill"
 		}
 	}
 	
 	@ViewBuilder
 	static func view(for tab: TabEnum) -> some View {
 		switch tab {
-        case .files: FilesView()
+		case .files: FilesView()
 		case .sources: SourcesView()
 		case .library: LibraryView()
 		case .settings: SettingsView()
 		case .certificates: NBNavigationView(.localized("Certificates")) { CertificatesView() }
 		case .appstore: AppstoreView()
-        case .downloader: DownloaderView()
+		case .downloader: DownloaderView()
 		}
 	}
 	
 	static var defaultTabs: [TabEnum] {
 		return [
-            .files,
-            .library,
-            .appstore,
-            .downloader,
+			.appstore,
+			.files,
+			.library,
+			.downloader,
 			.settings,
 		]
 	}
